2607b9d7ae2ac9d5f7cce275e0dd5c81b11a2083
Joshua Gassama <JoshuaGassama@yahoo.com>
2015-05-15 14:05:17 -0700

    Moved files from mariokart to our project

---
+++ js/resources.js
@@ -0,0 +1,33 @@
+game.resources = [
+
+	/* Graphics. 
+	 * @example
+	 * {name: "example", type:"image", src: "data/img/example.png"},
+	 */
+         {name: "main menu", type:"image", src: "data/img/mainmenu.jpg"}
+         
+         
+         
+         /* Atlases 
+	 * @example
+	 * {name: "example_tps", type: "tps", src: "data/img/example_tps.json"},
+	 */
+		
+	/* Maps. 
+	 * @example
+	 * {name: "example01", type: "tmx", src: "data/map/example01.tmx"},
+	 * {name: "example01", type: "tmx", src: "data/map/example01.json"},
+ 	 */
+
+	
+    
+         /* Background music. 
+	 * @example
+	 * {name: "example_bgm", type: "audio", src: "data/bgm/"},
+	 */	
+
+	/* Sound effects. 
+	 * @example
+	 * {name: "example_sfx", type: "audio", src: "data/sfx/"}
+	 */
+];
---
+++ screens/mainmenu.js
@@ -0,0 +1,67 @@
+game.mainmenu = me.ScreenObject.extend({
+	/**	
+	 *  action to perform on state change
+	 */
+	onResetEvent: function() {	
+		me.game.world.addChild(new me.Sprite(0, 0, me.loader.getImage('main menu')), -10); // TODO
+	
+                game.data.option1 = new (me.Renderable.extend({
+                    init: function(){
+                        this._super(me.Renderable, 'init', [270,  240,  300, 50]);
+                        this.font = new me.Font("Arial",  46, "white");
+                        me.input.registerPointerEvent('pointerdown', this, this.newGame.bind(this), true);
+                        },
+                    
+                    draw: function(renderer){
+                        this.font.draw(renderer.getContext(), "START A NEW GAME",  this.pos.x,  this.pos.y);
+                        },
+                        
+                       update: function(dt){
+                           return true;
+                       },
+                       
+                       newGame: function(){
+                           me.input.releasePointerEvent('pointerdown', this);
+                           me.input.releasePointerEvent('pointerdown', game.data.option2);
+                           me.state.change(me.state.NEW);
+                       }
+                   }));
+                   
+                   me.game.world.addChild(game.data.option1, 10);
+        
+                  game.data.option2=(new (me.Renderable.extend({
+                    init: function(){
+                        this._super(me.Renderable, 'init', [380,  340,  250, 50]);
+                        this.font = new me.Font("Arial",  46, "white");
+                        me.input.registerPointerEvent('pointerdown', this, this.newGame.bind(this), true);
+                        },
+                    
+                    draw: function(renderer){
+                        this.font.draw(renderer.getContext(), "CONTINUE YOUR GAME",  this.pos.x,  this.pos.y);
+                        },
+                        
+                       update: function(dt){
+                           return true;
+                       },
+                       
+                       newGame: function(){
+                          me.input.releasePointerEvent('pointerdown', game.data.option1);
+                          me.input.releasePointerEvent('pointerdown', this);
+                          me.state.change(me.state.LOAD);        
+                       }
+                })));
+                
+                me.game.world.addChild(game.data.option2);
+        
+        },
+	
+	
+	/**	
+	 *  action to perform when leaving this screen (state change)
+	 */
+	onDestroyEvent: function() {
+		
+	}
+});
+
+
